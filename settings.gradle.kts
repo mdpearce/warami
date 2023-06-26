c91345dc524e571b26b5e9558a4f09e1d6e5027a
Michael Pearce <mdpearce@gmail.com>
2023-06-26 19:52:37 +1000

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+@file:Suppress("UnstableApiUsage")
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "Warami"
+include(":app")
