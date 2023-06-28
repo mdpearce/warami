40a9d52f229e63b1899ac6136edb013974fcf18b
Michael Pearce <mdpearce@gmail.com>
2023-06-28 15:49:02 +1000

    Add API dependencies

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,9 +1,19 @@
 plugins {
     id("com.neaniesoft.warami.android-conventions")
+    kotlin("kapt")
 }
 
 android {
     defaultConfig {
         namespace = "com.neaniesoft.warami.api"
     }
+}
+
+dependencies {
+    implementation(libs.moshi)
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.converter.moshi)
+    implementation(libs.okhttp)
+    implementation(libs.okhttp.logging.interceptor)
+    kapt(libs.moshi.kotlin.codegen)
 }
