ffd75f4ba21d8bc4cca7d1f076809fd1a0f89bf7
Michael Pearce <mdpearce@gmail.com>
2023-07-01 17:42:20 +1000

    Add Post model

---
+++ common/src/main/java/com/neaniesoft/warami/common/models/Post.kt
@@ -0,0 +1,34 @@
+package com.neaniesoft.warami.common.models
+
+import java.time.LocalDateTime
+
+data class Post(
+    val id: PostId,
+    val name: String,
+    val creator: Person,
+    val community: Community,
+    val isRemoved: Boolean,
+    val isLocked: Boolean,
+    val publishedAt: LocalDateTime,
+    val isDeleted: Boolean,
+    val isNsfw: Boolean,
+    val apId: String,
+    val isLocal: Boolean,
+    val languageId: Int,
+    val isFeaturedCommunity: Boolean,
+    val url: UriString?,
+    val body: String?,
+    val updatedAt: LocalDateTime?,
+    val embedTitle: String?,
+    val embedDescription: String?,
+    val thumbnail: UriString?,
+    val embedVideo: UriString?,
+    val isCreatorBannedFromCommunity: Boolean,
+    val aggregates: PostAggregates,
+    val subscribedTyped: SubscribedType,
+    val isSaved: Boolean,
+    val isRead: Boolean,
+    val isCreatorBlocked: Boolean,
+    val myVote: Int?,
+    val searchParameters: PostSearchParameters
+)
---
+++ common/src/main/java/com/neaniesoft/warami/common/models/SubscribedType.kt
@@ -0,0 +1,7 @@
+package com.neaniesoft.warami.common.models
+
+enum class SubscribedType(val value: String) {
+    SUBSCRIBED("Subscribed"),
+    NOT_SUBSCRIBED("NotSubscribed"),
+    PENDING("Pending"),
+}
