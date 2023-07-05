a1351e2001627a97c8ca3fbd75761c1005b18e6c
Michael Pearce <mdpearce@gmail.com>
2023-07-05 17:14:46 +1000

    Add build conventions plugin for compose

---
+++ buildSrc/src/main/kotlin/com.neaniesoft.warami.android-compose-conventions.gradle.kts
@@ -0,0 +1,29 @@
+import org.gradle.accessors.dm.LibrariesForLibs
+
+plugins {
+    id("com.neaniesoft.warami.android-conventions")
+}
+
+val libs = the<LibrariesForLibs>()
+
+android {
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.kotlin.compiler.extension.get()
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+}
