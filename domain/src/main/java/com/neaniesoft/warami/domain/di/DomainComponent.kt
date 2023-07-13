5f77b081ac3c9c0a95c585e0d044081be19d6b2a
Michael Pearce <mdpearce@gmail.com>
2023-07-13 17:11:56 +1000

    get instances download url

--- domain/src/main/java/com/neaniesoft/warami/domain/di/DomainComponent.kt
+++ domain/src/main/java/com/neaniesoft/warami/domain/di/DomainComponent.kt
@@ -3,6 +3,7 @@ package com.neaniesoft.warami.domain.di
 import android.content.Context
 import com.neaniesoft.warami.data.di.DatabaseComponent
 import com.neaniesoft.warami.domain.usecases.BuildPostSearchParametersUseCase
+import com.neaniesoft.warami.domain.usecases.GetInstancesDownloadUrlUseCase
 import me.tatarka.inject.annotations.Component
 import me.tatarka.inject.annotations.Provides
 import me.tatarka.inject.annotations.Scope
@@ -17,6 +18,7 @@ abstract class DomainComponent(
     fun provideContext(): Context = dataComponent.provideContext()
 
     abstract val buildPostSearchParametersUseCase: BuildPostSearchParametersUseCase
+    abstract val getInstancesDownloadUrlUseCase: GetInstancesDownloadUrlUseCase
 }
 
 @Scope
