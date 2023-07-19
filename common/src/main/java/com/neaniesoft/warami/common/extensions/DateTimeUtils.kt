1348ae00734e8b95a0c3274475dbd15457ab918b
Michael Pearce <mdpearce@gmail.com>
2023-07-19 20:40:46 +1000

    Add format period function

--- common/src/main/java/com/neaniesoft/warami/common/extensions/DateTimeUtils.kt
+++ common/src/main/java/com/neaniesoft/warami/common/extensions/DateTimeUtils.kt
@@ -1,8 +1,11 @@
 package com.neaniesoft.warami.common.extensions
 
+import android.content.res.Resources
+import com.neaniesoft.warami.common.R
 import java.time.LocalDateTime
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
 
 fun String.parseLocalDateTime(formatter: DateTimeFormatter): LocalDateTime {
     return LocalDateTime.parse(this, formatter)
@@ -10,4 +13,24 @@ fun String.parseLocalDateTime(formatter: DateTimeFormatter): LocalDateTime {
 
 fun String.parseZonedDateTime(): ZonedDateTime {
     return ZonedDateTime.parse(this, DateTimeFormatter.ISO_ZONED_DATE_TIME)
+}
+
+fun ZonedDateTime.formatPeriod(resources: Resources, comparison: ZonedDateTime): String {
+    val minutes = ChronoUnit.MINUTES.between(this, comparison)
+    if (minutes < 1) {
+        return resources.getString(R.string.just_now)
+    }
+
+    val hours = ChronoUnit.HOURS.between(this, comparison)
+    if (hours < 1) {
+        return resources.getString(R.string.minutes_ago, minutes)
+    }
+
+    val days = ChronoUnit.DAYS.between(this, comparison)
+    if (days < 1) {
+        return resources.getString(R.string.hours_ago, hours)
+    }
+
+    return resources.getString(R.string.days_ago, days)
+
 }
