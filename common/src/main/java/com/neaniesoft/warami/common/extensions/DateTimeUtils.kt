eb54d5d854892d2d50f871337793eae4854ef43a
Michael Pearce <mdpearce@gmail.com>
2023-07-20 00:37:51 +1000

    Use quantity strings for display

--- common/src/main/java/com/neaniesoft/warami/common/extensions/DateTimeUtils.kt
+++ common/src/main/java/com/neaniesoft/warami/common/extensions/DateTimeUtils.kt
@@ -2,7 +2,6 @@ package com.neaniesoft.warami.common.extensions
 
 import android.content.res.Resources
 import com.neaniesoft.warami.common.R
-import java.time.Instant
 import java.time.LocalDateTime
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
@@ -24,14 +23,13 @@ fun ZonedDateTime.formatPeriod(resources: Resources, comparison: ZonedDateTime):
 
     val hours = ChronoUnit.HOURS.between(this, comparison)
     if (hours < 1) {
-        return resources.getString(R.string.minutes_ago, minutes)
+        return resources.getQuantityString(R.plurals.minutes_ago, minutes.toInt(), minutes.toInt())
     }
 
     val days = ChronoUnit.DAYS.between(this, comparison)
     if (days < 1) {
-        return resources.getString(R.string.hours_ago, hours)
+        return resources.getQuantityString(R.plurals.hours_ago, hours.toInt(), hours.toInt())
     }
 
-    return resources.getString(R.string.days_ago, days)
-
+    return resources.getQuantityString(R.plurals.days_ago, days.toInt(), days.toInt())
 }
